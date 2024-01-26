5cfcf4d8b730e1a89ed58fe07889461f150e1675
AlwaleedAlroomi <alwaleed101az@gmail.com>
2024-01-26 23:58:35 +0300

    change repo name when creating it

--- newrepo.py
+++ newrepo.py
@@ -9,19 +9,24 @@ token = env("gt")
 message = sys.argv[1]
 path = sys.argv[2]
 reponame = path.split('/')
+changename = input("Enter new repository name(no spaces in the name you can use - instead)\nor leave it blank and it will be the same as the folder name: ")
 
 github = Github(token)
 user = github.get_user()
 login = user.login
-repo = user.create_repo(reponame[-1])
+
+if changename == "":
+    repo = user.create_repo(reponame[-1])
+else:
+    repo = user.create_repo(changename)
 
 commands = [
-    f'echo"# {repo.name}" >> README.md',
+    f'echo "# {repo.name}" >> README.md',
     'git init',
     'git add *',
     f'git commit -m "{message}"',
     'git branch -M main',
-    f'git remote add origin https://github.com/{login}/{reponame[-1]}.git',
+    f'git remote add origin https://github.com/{login}/{repo.name}.git',
     'git push -u origin main',
     'code .',
 ]
