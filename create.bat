30129cc14f1483e05d0a21a047837ce2b224df5e
Alwaleed Alroomi <alwaleed101az@gmail.com>
2024-03-05 09:43:04 +0300

    var-error

--- create.bat
+++ create.bat
@@ -10,11 +10,11 @@ rem if the user entered a msg it will skip the if condition and enter else condi
 ) else (
     rem if the user want to make a new repo from zero he will only enter the msg
     if "%2" == "" (
-        python D:\Programming\python\Python-GitHub-Automation\newrepo.py "$msg" "%pa%"
+        python D:\Programming\python\Python-GitHub-Automation\newrepo.py "%msg%" "%pa%"
     ) else (
         rem if he wants to make a repo from a local file on the pc he will enter 'l' at the end
         if "%2" == "-l" (
-            python D:\Programming\python\Python-GitHub-Automation\localrepo.py "$msg"
+            python D:\Programming\python\Python-GitHub-Automation\localrepo.py "%msg%"
         )
     )
 )
