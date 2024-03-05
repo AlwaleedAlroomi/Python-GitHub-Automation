c02a9074541ba85cd52a3cd226fd3423b9968e6d
Alwaleed Alroomi <alwaleed101az@gmail.com>
2024-03-05 09:38:14 +0300

    solve

--- create.bat
+++ create.bat
@@ -3,18 +3,20 @@
 set pa=%CD%
 
 rem it will check if the user entered a folder msg 
+echo "else"
 if "%1" == "" (
+    echo "after"
     rem if there is no msg it will print error
-    echo "error you did not enter a msg for the commit"
+    echo "error you did not enter a message for the commit"
 rem if the user entered a msg it will skip the if condition and enter else condition
 ) else (
     rem if the user want to make a new repo from zero he will only enter the msg
     if "%2" == "" (
-        python D:\Programming\python\Python-GitHub-Automation\newremote.py "%1" %pa%
+        python D:\Programming\python\Python-GitHub-Automation\newrepo.py "%1" "%pa%"
     ) else (
         rem if he wants to make a repo from a local file on the pc he will enter 'l' at the end
         if "%2" == "-l" (
-            python D:\Programming\python\Python-GitHub-Automation\localrepo.py "%1" 
+            python D:\Programming\python\Python-GitHub-Automation\localrepo.py "%1"
         )
     )
 )
--- newrepo.py
+++ newrepo.py
@@ -8,15 +8,17 @@ env.read_env()
 token = env("gt")
 message = sys.argv[1]
 path = sys.argv[2]
-reponame = path.split('/')
-changename = input(f"Enter new repository name(no spaces in the name you can use - instead)\nor leave it blank and it will be {reponame[-1]}: ")
+if os.name == "nt": # if windows
+    reponame = path.split('\\')[-1]
+reponame = path.split('/')[-1]
+changename = input(f"Enter new repository name(no spaces in the name you can use - instead)\nor leave it blank and it will be {reponame}: ")
 
 github = Github(token)
 user = github.get_user()
 login = user.login
 
 if changename == "":
-    repo = user.create_repo(reponame[-1])
+    repo = user.create_repo(reponame)
 else:
     repo = user.create_repo(changename)
 
@@ -32,12 +34,21 @@ commands = [
 ]
 
 # Check if the folder contains a README.md file or not
+if os.name == 'nt':
+    if not os.path.exists(path + "\README.md"):
+        for c in commands:
+            os.system(c)
+    else:
+        commands.pop(0)
+        for c in commands:
+            os.system(c)
+
 if not os.path.exists(path + "/README.md"):
     for c in commands:
         os.system(c)
-else:
-    commands.pop(0)
-    for c in commands:
-        os.system(c)
+
+commands.pop(0)
+for c in commands:
+    os.system(c)
 
 print('Pushing data Done!')
